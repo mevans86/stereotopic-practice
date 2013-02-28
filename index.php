0573d0f0b7ec7986cf188693b93aad707a85c334
Michael Evans <mevans@metallacycle.com>
2013-02-27 20:30:08 -0600

    Intro message, problem navigation, feedback for first 8 problems
    Intro message provided when no valid problem parameter is supplied,
    problems can be randomly navigated, and 8 problems are fully implemented

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
 <?
-	$problemId = -1;
-	$compoundName = "4-<i>tert</i>-butylcyclohexanone";
+	do {
+		$randomId = mt_rand(1, 8);
+	} while ($randomId == $_REQUEST["p"]);
 	if((int)$_REQUEST["p"]) {
 		$problemId = (int)$_REQUEST["p"];
 		$f = fopen("problems.txt", "r");
 		$contents = fread($f, filesize("problems.txt"));
 		$problems = preg_split("/\n/", $contents);
-		$problem = preg_split("/\t/", $problems[$problemId]);
-		$compoundName = $problem[0];
-		$correctRelationship = $problem[1];
-		$incorrectFeedback = $problem[2];
-		$correctFeedback = $problem[3];
+		
+		if(count($problems) > $problemId and $problemId >= 1) {
+			$problem = preg_split("/\t/", $problems[$problemId]);
+			$compoundName = $problem[0];
+			$correctRelationship = $problem[1];
+			$incorrectFeedback = $problem[2];
+			$correctFeedback = $problem[3];
+		} else {
+			$intro = true;
+		}
+	} else {
+		$intro = true;
 	}
 ?>
 <!DOCTYPE HTML>
@@ -20,6 +28,7 @@
 	<title>Coursera | Stereotopic Relationships</title>
 	<script type="text/javascript" src="http://www.metallacycle.com/play/coursera/lib/jquery.min.js"></script>
 	<script type="text/javascript" src="http://www.metallacycle.com/play/coursera/lib/jsmol/JSmol.min.js"></script>
+	<script type="text/javascript" src="http://www.metallacycle.com/play/coursera/lib/bootstrap/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="http://www.metallacycle.com/play/coursera/lib/bootstrap/css/bootstrap.min.css" />
 	
 	<style type="text/css">
@@ -35,46 +44,54 @@
 <body>
 	<div class="container">
 		<div class="page-header">
-			<h1>Practice Identifying Stereotopic Relationships<br>
-			<small>template page</small></h1>
-			
-			<div class="span11">
-				<h2><? echo $compoundName; ?></h2>
-				<p>
-					<div class="q-prompt">
-						What is the stereotopic relationship between the highlighted atoms/groups in the molecule shown? Select a relationship below.
-					</div>
-					<div data-toggle="buttons-radio" class="mc-options btn-group">
-						<button class="btn" id="homotopic" onclick="evaluate_response($(this).attr('id'));">Homotopic</button>
-						<button class="btn" id="enantiotopic" onclick="evaluate_response($(this).attr('id'));">Enantiotopic</button>
-						<button class="btn" id="diastereotopic" onclick="evaluate_response($(this).attr('id'));">Diastereotopic</button>
-						<button class="btn" id="constitutionally-heterotopic" onclick="evaluate_response($(this).attr('id'));">Constitutionally Heterotopic</button>
-					</div>
-				</p>
-				<div class="mol-image pull-right">
-					<img src="http://www.metallacycle.com/play/coursera/assets/images/stereo/<? echo $problemId; ?>.png" alt="A Lewis structure of 4-tert-butylcyclohexanone. One hydrogen on each alpha position is highlighted orange." />
+			<h1>Stereotopicity Trainer<br>
+			<small>practice identifying stereotopic relationships</small></h1>
+		</div>
+		
+		<div>
+			<?
+				if($intro) {
+					echo '<p>Each problem on this page includes a molecule with two groups or atoms highlighted. Your task is to identify the stereotopic relationship shared by the groups, using either the <a target="_blank" href="http://www.youtube.com/watch?v=WIVJPBBUGsE">Q-test</a> or <a target="_blank" href="http://www.youtube.com/watch?v=facIqMwAFec">symmetry elements</a>. Examine the two- and three-dimensional structures provided, then select a relationship. Immediate feedback will be provided, and you can attempt each problem as many times as you\'d like. Click the button below to begin practicing.</p><p>There are <b>8</b> problems currently available in the Stereotopicity Trainer.</p>';
+					echo '<a class="btn btn-large btn-primary" href=".?p='.$randomId.'">Start Practicing!</a>';
+					die('</div></div><footer><small>Created by <a href="http://www.metallacycle.com/">Michael Evans</a> in 2013.</small></footer>');
+				}
+			?>
+			<? echo '<div class="btn-group"><a class="btn btn-small btn-primary" href=".">Home</a><a class="btn btn-small btn-primary" href=".?p='.$randomId.'">Next Problem</a></div>'; ?>
+			<h2><? echo $compoundName; ?></h2>
+			<p>
+				<div class="q-prompt">
+					What is the stereotopic relationship between the highlighted atoms/groups in the molecule shown? Consider the most symmetric conformer(s) of the molecule. Select a relationship below.
 				</div>
-				<div class="jsmol well" style="width:460px;">
-					<script type="text/javascript">
-						var jmol = 'jmol';
-						var jmolOptions = {
-							width: 460,
-							height: 400,
-							script: 'set background [242,242,242]; load http://www.metallacycle.com/play/coursera/assets/images/stereo/<? echo $problemId; ?>.jmol',
-							jarPath: 'http://www.metallacycle.com/play/coursera/lib/jsmol/java',
-							jarFile: 'JmolAppletSigned.jar',
-							isSigned: true,
-							j2sPath: 'http://www.metallacycle.com/play/coursera/lib/jsmol/j2s',
-							use: 'HTML5',
-							console: 'console-messages' };
-						jmol = Jmol.getApplet(jmol, jmolOptions);
-					</script>
+				<div data-toggle="buttons-radio" class="mc-options btn-group">
+					<button class="btn" id="homotopic" onclick="evaluate_response($(this).attr('id'));">Homotopic</button>
+					<button class="btn" id="enantiotopic" onclick="evaluate_response($(this).attr('id'));">Enantiotopic</button>
+					<button class="btn" id="diastereotopic" onclick="evaluate_response($(this).attr('id'));">Diastereotopic</button>
+					<button class="btn" id="constitutionally-heterotopic" onclick="evaluate_response($(this).attr('id'));">Constitutionally Heterotopic</button>
 				</div>
+			</p>
+			<div class="mol-image pull-right">
+				<img src="http://www.metallacycle.com/play/coursera/assets/images/stereo/<? echo $problemId; ?>.png" alt="A Lewis structure of 4-tert-butylcyclohexanone. One hydrogen on each alpha position is highlighted orange." />
+			</div>
+			<div class="jsmol well" style="width:460px;">
+				<script type="text/javascript">
+					var jmol = 'jmol';
+					var jmolOptions = {
+						width: 460,
+						height: 400,
+						script: 'set background [242,242,242]; load http://www.metallacycle.com/play/coursera/assets/images/stereo/<? echo $problemId; ?>.jmol',
+						jarPath: 'http://www.metallacycle.com/play/coursera/lib/jsmol/java',
+						jarFile: 'JmolAppletSigned.jar',
+						isSigned: true,
+						j2sPath: 'http://www.metallacycle.com/play/coursera/lib/jsmol/j2s',
+						use: 'HTML5',
+						console: 'console-messages' };
+					jmol = Jmol.getApplet(jmol, jmolOptions);
+				</script>
 			</div>
 		</div>
 	</div>
 	<footer><small>Created by <a href="http://www.metallacycle.com/">Michael Evans</a> in 2013.</small></footer>
-	<script type="text/javascript" src="http://www.metallacycle.com/play/coursera/lib/bootstrap/js/bootstrap.min.js"></script>
+	
 	<script type="text/javascript">
 		var attempt = 1;
 		function evaluate_response(res) {
